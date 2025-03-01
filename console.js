f8a1884ead1c2350b5fd5d0b256d7c32a1d073c2
krishna4062 <krishna.shashank894@gmail.com>
2025-03-01 14:50:12 +0530

    code change

--- console.js
+++ console.js
@@ -1,3 +1,20 @@
-let request = "The status code is 200 OK"; 
+function Fun1() {
+    for (x = 0; x < 10; x++){
+                if (x %2 == 0) {
+            console.log("Even numbers: ", x) 
+         }
+    }
+}
 
-console.log(request.length);
+
+
+function Fun2() {
+    for (y = 1; y < 10; y++){
+        
+        if (y %2 == 1) {
+            console.log("Odd numbers: ", y) 
+         }
+    }
+}
+Fun1();
+Fun2();
